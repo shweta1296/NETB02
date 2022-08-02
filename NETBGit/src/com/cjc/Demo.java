2c26714b9d47278c8eb670b03b5ddb038a69b94b
Sayalibd <110378716+Sayalibd@users.noreply.github.com>
2022-08-02 14:37:39 +0530

    By sayali

--- NETBGit/src/com/cjc/Demo.java
+++ NETBGit/src/com/cjc/Demo.java
@@ -9,6 +9,12 @@ public class Demo {
 	{
 		System.out.println("m2ooo");
 	}
+	public void m3()
+	{
+		System.out.println("m3----1");
+		System.out.println("m3---2");
+	}
+	
 	public static void main(String args[])
 	{
 		
